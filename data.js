169a2a6cc168e6f7be18ee84d344c4e8a7f16454
Vlad8001 <71021816+Vlad8001@users.noreply.github.com>
2026-05-14 21:23:38 +0300

    Update data.js (2026-05-14T18:23:49.359Z)

--- data.js
+++ data.js
@@ -1,329 +1,421 @@
 window.appData = {
-  "lastUpdated": "2026-05-14T17:13:40.734Z",
-  "reports": [
+  "lastUpdated": "2026-05-14T18:02:44.229Z",
+  "campaigns": [
     {
-      "date": "2026-05-14",
-      "time": "",
-      "campaigns": [
-        {
-          "name": "Mechanical Companies (Vancouver)",
-          "tag": "PREV",
-          "metrics": {
-            "contacts": {
-              "value": 34,
-              "delta": 0
-            },
-            "notAddedToSequence": {
-              "value": 0,
-              "delta": 0
-            },
-            "inbox": {
-              "value": 34,
-              "delta": 0
-            },
-            "sent": {
-              "value": 34,
-              "delta": 0
-            },
-            "delivered": {
-              "value": 28,
-              "delta": 0
-            },
-            "bounced": {
-              "value": 5,
-              "delta": 0
-            },
-            "spamBlocked": {
-              "value": 1,
-              "delta": 0
-            },
-            "replies": {
-              "value": 7,
-              "delta": 0
-            },
-            "replyRate": {
-              "value": 25.9,
-              "delta": 0
-            },
-            "rfqReceived": {
-              "value": 1,
-              "delta": 0
-            }
-          }
+      "id": "ca3iwnjra",
+      "name": "New campaign",
+      "tag": "",
+      "stage": "active",
+      "metrics": {
+        "contacts": {
+          "value": 0,
+          "delta": 0
         },
-        {
-          "name": "Electrical Companies (Vancouver)",
-          "tag": "PREV",
-          "metrics": {
-            "contacts": {
-              "value": 28,
-              "delta": 0
-            },
-            "notAddedToSequence": {
-              "value": 0,
-              "delta": 0
-            },
-            "inbox": {
-              "value": 28,
-              "delta": 0
-            },
-            "sent": {
-              "value": 28,
-              "delta": 0
-            },
-            "delivered": {
-              "value": 28,
-              "delta": 0
-            },
-            "bounced": {
-              "value": 0,
-              "delta": 0
-            },
-            "replies": {
-              "value": 4,
-              "delta": 0
-            },
-            "replyRate": {
-              "value": 14.3,
-              "delta": 0
-            },
-            "rfqReceived": {
-              "value": 1,
-              "delta": 0
-            }
-          }
+        "notAddedToSequence": {
+          "value": 0,
+          "delta": 0
         },
-        {
-          "name": "GC (Vancouver)",
-          "tag": "PREV",
-          "metrics": {
-            "contacts": {
-              "value": 83,
-              "delta": 0
-            },
-            "notAddedToSequence": {
-              "value": 0,
-              "delta": 0
-            },
-            "inbox": {
-              "value": 83,
-              "delta": 0
-            },
-            "sent": {
-              "value": 83,
-              "delta": 0
-            },
-            "delivered": {
-              "value": 101,
-              "delta": 8
-            },
-            "bounced": {
-              "value": 6,
-              "delta": 0
-            },
-            "replies": {
-              "value": 6,
-              "delta": 1
-            },
-            "spamBlocked": {
-              "value": 1,
-              "delta": 0
-            },
-            "replyRate": {
-              "value": 7.9,
-              "delta": 1.3
-            },
-            "rfqReceived": {
-              "value": 3,
-              "delta": 0
-            }
-          }
+        "inbox": {
+          "value": 0,
+          "delta": 0
         },
-        {
-          "name": "GC 100+ employees in Ontario - Intro",
-          "tag": "NOVA TOR",
-          "metrics": {
-            "contacts": {
-              "value": 46,
-              "delta": 0
-            },
-            "notAddedToSequence": {
-              "value": 0,
-              "delta": -1
-            },
-            "inbox": {
-              "value": 46,
-              "delta": 1
-            },
-            "sent": {
-              "value": 46,
-              "delta": 1
-            },
-            "delivered": {
-              "value": 41,
-              "delta": 1
-            },
-            "bounced": {
-              "value": 4,
-              "delta": 0
-            },
-            "spamBlocked": {
-              "value": 1,
-              "delta": 0
-            },
-            "replies": {
-              "value": 0,
-              "delta": 0
-            },
-            "replyRate": {
-              "value": 0,
-              "delta": 0
-            },
-            "rfqReceived": {
-              "value": 0,
-              "delta": 0
-            }
-          }
+        "sent": {
+          "value": 0,
+          "delta": 0
         },
-        {
-          "name": "New Electrical companies (Follow ups)",
-          "tag": "NOVA TOR",
-          "metrics": {
-            "contacts": {
-              "value": 100,
-              "delta": 0
-            },
-            "notAddedToSequence": {
-              "value": 20,
-              "delta": -10
-            },
-            "inbox": {
-              "value": 80,
-              "delta": 10
-            },
-            "sent": {
-              "value": 80,
-              "delta": 10
-            },
-            "delivered": {
-              "value": 72,
-              "delta": 10
-            },
-            "bounced": {
-              "value": 2,
-              "delta": 0
-            },
-            "spamBlocked": {
-              "value": 1,
-              "delta": 0
-            },
-            "badData": {
-              "value": 2,
-              "delta": 0
-            },
-            "replies": {
-              "value": 1,
-              "delta": 0
-            },
-            "replyRate": {
-              "value": 1.38,
-              "delta": -0.23
-            },
-            "rfqReceived": {
-              "value": 1,
-              "delta": 0
-            }
-          }
+        "delivered": {
+          "value": 0,
+          "delta": 0
         },
-        {
-          "name": "New General Contractors (com renos, GTA) (Follow Ups)",
-          "tag": "NOVA TOR",
-          "metrics": {
-            "contacts": {
-              "value": 69,
-              "delta": 0
-            },
-            "notAddedToSequence": {
-              "value": 0,
-              "delta": 0
-            },
-            "inbox": {
-              "value": 66,
-              "delta": 0
-            },
-            "sent": {
-              "value": 66,
-              "delta": 0
-            },
-            "delivered": {
-              "value": 63,
-              "delta": 0
-            },
-            "bounced": {
-              "value": 2,
-              "delta": 0
-            },
-            "replies": {
-              "value": 1,
-              "delta": 0
-            },
-            "replyRate": {
-              "value": 1.6,
-              "delta": 0
-            },
-            "rfqReceived": {
-              "value": 0,
-              "delta": 0
-            }
-          }
+        "bounced": {
+          "value": 0,
+          "delta": 0
         },
-        {
-          "name": "new GPR coring companies in US",
-          "tag": "RADII-X",
-          "metrics": {
-            "contacts": {
-              "value": 87,
-              "delta": 13
-            },
-            "notAddedToSequence": {
-              "value": 47,
-              "delta": -10
-            },
-            "inbox": {
-              "value": 40,
-              "delta": 10
-            },
-            "sent": {
-              "value": 40,
-              "delta": 10
-            },
-            "delivered": {
-              "value": 38,
-              "delta": 10
-            },
-            "bounced": {
-              "value": 1,
-              "delta": 0
-            },
-            "badData": {
-              "value": 1,
-              "delta": 0
-            },
-            "replies": {
-              "value": 1,
-              "delta": 1
-            },
-            "replyRate": {
-              "value": 2.6,
-              "delta": 2.6
-            },
-            "rfqReceived": {
-              "value": 1,
-              "delta": 1
-            }
-          }
+        "spamBlocked": {
+          "value": 0,
+          "delta": 0
+        },
+        "badData": {
+          "value": 0,
+          "delta": 0
+        },
+        "replies": {
+          "value": 0,
+          "delta": 0
+        },
+        "replyRate": {
+          "value": 0,
+          "delta": 0
+        },
+        "rfqReceived": {
+          "value": 0,
+          "delta": 0
         }
-      ]
+      }
+    },
+    {
+      "name": "Mechanical Companies (Vancouver)",
+      "tag": "PREV",
+      "metrics": {
+        "contacts": {
+          "value": 34,
+          "delta": 0
+        },
+        "notAddedToSequence": {
+          "value": 0,
+          "delta": 0
+        },
+        "inbox": {
+          "value": 34,
+          "delta": 0
+        },
+        "sent": {
+          "value": 34,
+          "delta": 0
+        },
+        "delivered": {
+          "value": 28,
+          "delta": 0
+        },
+        "bounced": {
+          "value": 5,
+          "delta": 0
+        },
+        "spamBlocked": {
+          "value": 1,
+          "delta": 0
+        },
+        "replies": {
+          "value": 7,
+          "delta": 0
+        },
+        "replyRate": {
+          "value": 25.9,
+          "delta": 0
+        },
+        "rfqReceived": {
+          "value": 1,
+          "delta": 0
+        },
+        "badData": {
+          "value": 0,
+          "delta": 0
+        }
+      },
+      "id": "cxlzweagd",
+      "stage": "followup"
+    },
+    {
+      "name": "Electrical Companies (Vancouver)",
+      "tag": "PREV",
+      "metrics": {
+        "contacts": {
+          "value": 28,
+          "delta": 0
+        },
+        "notAddedToSequence": {
+          "value": 0,
+          "delta": 0
+        },
+        "inbox": {
+          "value": 28,
+          "delta": 0
+        },
+        "sent": {
+          "value": 28,
+          "delta": 0
+        },
+        "delivered": {
+          "value": 28,
+          "delta": 0
+        },
+        "bounced": {
+          "value": 0,
+          "delta": 0
+        },
+        "replies": {
+          "value": 4,
+          "delta": 0
+        },
+        "replyRate": {
+          "value": 14.3,
+          "delta": 0
+        },
+        "rfqReceived": {
+          "value": 1,
+          "delta": 0
+        },
+        "spamBlocked": {
+          "value": 0,
+          "delta": 0
+        },
+        "badData": {
+          "value": 0,
+          "delta": 0
+        }
+      },
+      "id": "cw861mwgc",
+      "stage": "followup"
+    },
+    {
+      "name": "GC (Vancouver)",
+      "tag": "PREV",
+      "metrics": {
+        "contacts": {
+          "value": 83,
+          "delta": 0
+        },
+        "notAddedToSequence": {
+          "value": 0,
+          "delta": 0
+        },
+        "inbox": {
+          "value": 83,
+          "delta": 0
+        },
+        "sent": {
+          "value": 83,
+          "delta": 0
+        },
+        "delivered": {
+          "value": 101,
+          "delta": 8
+        },
+        "bounced": {
+          "value": 6,
+          "delta": 0
+        },
+        "replies": {
+          "value": 6,
+          "delta": 1
+        },
+        "spamBlocked": {
+          "value": 1,
+          "delta": 0
+        },
+        "replyRate": {
+          "value": 7.9,
+          "delta": 1.3
+        },
+        "rfqReceived": {
+          "value": 3,
+          "delta": 0
+        },
+        "badData": {
+          "value": 0,
+          "delta": 0
+        }
+      },
+      "id": "c4o4gvb5t",
+      "stage": "followup"
+    },
+    {
+      "name": "GC 100+ employees in Ontario - Intro",
+      "tag": "NOVA TOR",
+      "metrics": {
+        "contacts": {
+          "value": 46,
+          "delta": 0
+        },
+        "notAddedToSequence": {
+          "value": 0,
+          "delta": -1
+        },
+        "inbox": {
+          "value": 46,
+          "delta": 1
+        },
+        "sent": {
+          "value": 46,
+          "delta": 1
+        },
+        "delivered": {
+          "value": 41,
+          "delta": 1
+        },
+        "bounced": {
+          "value": 4,
+          "delta": 0
+        },
+        "spamBlocked": {
+          "value": 1,
+          "delta": 0
+        },
+        "replies": {
+          "value": 0,
+          "delta": 0
+        },
+        "replyRate": {
+          "value": 0,
+          "delta": 0
+        },
+        "rfqReceived": {
+          "value": 0,
+          "delta": 0
+        },
+        "badData": {
+          "value": 0,
+          "delta": 0
+        }
+      },
+      "id": "cctmijqqu",
+      "stage": "followup"
+    },
+    {
+      "name": "New Electrical companies (Follow ups)",
+      "tag": "NOVA TOR",
+      "metrics": {
+        "contacts": {
+          "value": 100,
+          "delta": 0
+        },
+        "notAddedToSequence": {
+          "value": 20,
+          "delta": -10
+        },
+        "inbox": {
+          "value": 80,
+          "delta": 10
+        },
+        "sent": {
+          "value": 80,
+          "delta": 10
+        },
+        "delivered": {
+          "value": 72,
+          "delta": 10
+        },
+        "bounced": {
+          "value": 2,
+          "delta": 0
+        },
+        "spamBlocked": {
+          "value": 1,
+          "delta": 0
+        },
+        "badData": {
+          "value": 2,
+          "delta": 0
+        },
+        "replies": {
+          "value": 1,
+          "delta": 0
+        },
+        "replyRate": {
+          "value": 1.38,
+          "delta": -0.23
+        },
+        "rfqReceived": {
+          "value": 1,
+          "delta": 0
+        }
+      },
+      "id": "cfw4uqya6",
+      "stage": "followup"
+    },
+    {
+      "name": "New General Contractors (com renos, GTA) (Follow Ups)",
+      "tag": "NOVA TOR",
+      "metrics": {
+        "contacts": {
+          "value": 69,
+          "delta": 0
+        },
+        "notAddedToSequence": {
+          "value": 0,
+          "delta": 0
+        },
+        "inbox": {
+          "value": 66,
+          "delta": 0
+        },
+        "sent": {
+          "value": 66,
+          "delta": 0
+        },
+        "delivered": {
+          "value": 63,
+          "delta": 0
+        },
+        "bounced": {
+          "value": 2,
+          "delta": 0
+        },
+        "replies": {
+          "value": 1,
+          "delta": 0
+        },
+        "replyRate": {
+          "value": 1.6,
+          "delta": 0
+        },
+        "rfqReceived": {
+          "value": 0,
+          "delta": 0
+        },
+        "spamBlocked": {
+          "value": 0,
+          "delta": 0
+        },
+        "badData": {
+          "value": 0,
+          "delta": 0
+        }
+      },
+      "id": "cl8oe1unv",
+      "stage": "followup"
+    },
+    {
+      "name": "new GPR coring companies in US",
+      "tag": "RADII-X",
+      "metrics": {
+        "contacts": {
+          "value": 87,
+          "delta": 13
+        },
+        "notAddedToSequence": {
+          "value": 47,
+          "delta": -10
+        },
+        "inbox": {
+          "value": 40,
+          "delta": 10
+        },
+        "sent": {
+          "value": 40,
+          "delta": 10
+        },
+        "delivered": {
+          "value": 38,
+          "delta": 10
+        },
+        "bounced": {
+          "value": 1,
+          "delta": 0
+        },
+        "badData": {
+          "value": 1,
+          "delta": 0
+        },
+        "replies": {
+          "value": 1,
+          "delta": 1
+        },
+        "replyRate": {
+          "value": 2.6,
+          "delta": 2.6
+        },
+        "rfqReceived": {
+          "value": 1,
+          "delta": 1
+        },
+        "spamBlocked": {
+          "value": 0,
+          "delta": 0
+        }
+      },
+      "id": "ccxu2ja4s",
+      "stage": "followup"
     }
   ]
 };
